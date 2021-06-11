86f799e02ae05d10b3793c4059855321b9297f47
Caseywalker <casey.walker118@gmail.com>
2021-06-11 14:08:58 -0500

    savings functions

--- src/views/Savings.js
+++ src/views/Savings.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import SavingsForm from '../components/SavingsForm';
 
-export default function Savings() {
+function Savings({
+  user,
+  setSavings
+}) {
   return (
     <div>
       <h2>You are on the Savings page</h2>
+      <SavingsForm
+        formTitle={'Add Saving'}
+        setSavings={setSavings}
+        user={user}
+      />
     </div>
   );
 }
+
+Savings.propTypes = {
+  user: PropTypes.any,
+  setSavings: PropTypes.func.isRequired,
+  savings: PropTypes.array.isRequired
+};
+
+export default Savings;
